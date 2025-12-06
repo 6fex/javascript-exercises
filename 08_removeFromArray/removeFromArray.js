4b6882a0c3626f2bbcbb8be7f52ebd90d680ced5
6fex <54210730+6fex@users.noreply.github.com>
2025-12-06 23:06:48 +0100

    Complete the 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    const copyToFilter = array.slice();
+    remove.forEach(toRemove => {
+        while (copyToFilter.includes(toRemove)) {
+            let index = copyToFilter.findIndex(item => item === toRemove);
+            copyToFilter.splice(index, 1);
+        }
+    });
+    return copyToFilter;
 };
 
 // Do not edit below this line
