dc572bff3a2dce73995e0441396d300dc9833357
6fex <54210730+6fex@users.noreply.github.com>
2025-12-16 12:34:43 +0100

    Complete the 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((obj, element) => {
+        if (!("yearOfDeath" in obj)) obj.yearOfDeath = (new Date()).getFullYear();
+        if (!("yearOfDeath" in element)) element.yearOfDeath = (new Date()).getFullYear();
 
+        function lifespan(person) {
+            return person.yearOfDeath - person.yearOfBirth;
+        };
+
+        return lifespan(obj) > lifespan(element) ? obj : element;
+    });
 };
 
 // Do not edit below this line
