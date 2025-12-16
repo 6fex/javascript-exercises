9288de080c6a46d726c7379f567d5508d77e388f
6fex <54210730+6fex@users.noreply.github.com>
2025-12-16 11:59:33 +0100

    Complete the 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    const fib = [0, 1];
+    
+    for (let i = 0; i < number - 1; i++) {
+        const nextInSequence = fib[fib.length - 1] + fib[fib.length - 2];
+        fib.push(nextInSequence);
+    }
 
+    return fib[number];
 };
-
+console.log(fibonacci("21"))
 // Do not edit below this line
 module.exports = fibonacci;
