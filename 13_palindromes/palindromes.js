6a41744b275f363c53570df52ac35284ba1e5d7b
6fex <54210730+6fex@users.noreply.github.com>
2025-12-16 11:24:31 +0100

    Complete the 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lowerCaseNoSpace = string.toLowerCase()
+                                        .split(" ")
+                                        .join("");
+    const filteredPunctuation = lowerCaseNoSpace.split("")
+                                                    .filter(character => /[a-z0-9]/.test(character))
+                                                    .join("");
+    const reversed = filteredPunctuation.split("")
+                                            .reverse()
+                                            .join("");
+    
+    return filteredPunctuation === reversed ? true : false;
 };
 
 // Do not edit below this line
