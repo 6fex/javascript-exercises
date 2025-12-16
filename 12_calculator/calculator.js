48fb233b22d2ad2b86285cb9b37cb4967b947286
6fex <54210730+6fex@users.noreply.github.com>
2025-12-16 10:55:22 +0100

    Complete the 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,27 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    return array.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.reduce((acc, num) => acc * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fac = 1;
+    for (let i = a; i > 0; i--) {
+        fac *= i;
+    };
+    return fac;
 };
-
+console.log(sum([7, 11]));
 // Do not edit below this line
 module.exports = {
   add,
